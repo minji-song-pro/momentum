819d5fc80ac2be2aaccd0d4cf61cc259de05a5e5
Minji Song <66244682+minji-song-pro@users.noreply.github.com>
2022-11-11 19:43:00 +0900

    명언 한글 변경
    명언 한글 변경

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 $(document).ready(function () {
     renderCurrentTime();
     renderQuote();
+    getDday();
 });
 
 
 function renderCurrentTime() {
-let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
-fetch(url)
-.then(res => res.json()).then((data) => {
-    let datetime = data['datetime'].substr(11,5);
-    $('#time').text(datetime);
-});
-}
-
-
-function renderQuote() {
-    let url = `https://api.quotable.io/random`;
+    let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
     fetch(url)
         .then(res => res.json()).then((data) => {
-            let content = `" ${data['content']} "`;
-            let author = `- ${data['author']} -`;
-            $('#content').text(content);
-            $('#author').text(author);
+            let datetime = data['datetime'].substr(11, 5);
+            $('#time').text(datetime);
         });
+}
+
+function renderQuote() {
+    let url = `https://api.qwer.pw/request/helpful_text?apikey=guest`;
+    // let url = `https://api.quotable.io/random`;
+    fetch(url)
+        .then(res => res.json()).then((data) => {
+            let content = `${data[1]['respond']}`;
+            var contents = content.split('-');
+            var contents2 = null;
+
+            if (contents[1] == null) {
+                var contents2 = contents[0].split('–');
+                $('#content').text('" ' + contents2[0] + ' "');
+                $('#author').text(contents2[1]);
+            }
+            else {
+                $('#content').text('" ' + contents[0] + ' "');
+                $('#author').text(contents[1]);
+            }
+        });
+}
+
+function getDday() {
+    var today = new Date().getDate();
+    var test_date = 24 - today;
+    $('#dday').text("D-" + test_date);
+
 }
